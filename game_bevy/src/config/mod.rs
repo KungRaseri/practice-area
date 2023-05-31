2be704189177df3d53740eb2d05e870522bf6b2a
KungRaseri <countjoshington@gmail.com>
2023-05-30 22:27:51 -0700

    some more work on modularizing things, bringing in some example code for menus, etc.

---
+++ game_bevy/src/config/mod.rs
@@ -0,0 +1,10 @@
+use bevy::prelude::*;
+
+pub mod player_config {
+    pub const SPEED: f32 = SPRITE_SIZE * SPRITE_SIZE;
+    pub const SPRITE_SIZE: f32 = 16.0;
+}
+
+pub mod menu_config {}
+
+pub const TEXT_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
